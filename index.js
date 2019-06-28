d8436efb6c252188e227fd653e7186877cd7000e
Koua Thao <kouathao09@gmail.com>
2019-06-27 20:55:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Excerise from Angela Yu
+
+// $("button").click(function() {
+//   $("h1").css("color", "purple");
+// });
+
+// $("button").on("click", function() {
+//   $("h1").slideToggle();
+// });
+
+// $("button").on("click", function() {
+//   $("h1")
+//     .slideUp()
+//     .slideDown()
+//     .animate({ opacity: 0.5 });
+// });
+
+// Bootcamp 2019
+
+$("div").css("background-color", "purple");
+$(".highlight").css("width", "200px");
+// $("#third").css("border-color", "orange");
+$("#third").css("border", "orange solid 2px");
+$("div:first").css("color", "pink");
